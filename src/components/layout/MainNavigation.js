a97eeead4a7886a403f2f06d7288286a2b80d1a7
samy884 <ssgill884@gmail.com>
2022-06-25 12:44:08 +0530

    added mainnavigation classes

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,16 +1,21 @@
 import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   return (
-    <header>
-      <div>Great Quotes</div>
-      <nav>
+    <header className={classes.header}>
+      <div className={classes.logo}>Great Quotes</div>
+      <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to="/quotes">All Quotes</NavLink>
+            <NavLink to="/quotes" activeClassName={classes.active}>
+              All Quotes
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/new-quote">New Quote</NavLink>
+            <NavLink to="/new-quote" activeClassName={classes.active}>
+              New Quote
+            </NavLink>
           </li>
         </ul>
       </nav>
