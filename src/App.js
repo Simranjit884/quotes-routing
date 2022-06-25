086238b516d444b81276963d76180f59bec868ae
samy884 <ssgill884@gmail.com>
2022-06-25 10:45:33 +0530

    defining routing paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import Card from './components/UI/Card';
+import { Route } from "react-router-dom";
+import AllQuotes from "./components/pages/AllQuotes";
+import NewQuote from "./components/pages/NewQuote";
+import QuoteDetails from "./components/pages/QuoteDetails";
 
 function App() {
   return (
     <div>
+      <Route path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quotesId">
+        <QuoteDetails />
+      </Route>
+      <Route path="/new-quotes">
+        <NewQuote />
+      </Route>
     </div>
   );
 }
