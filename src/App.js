09cd687f9584fdd35b1debe2ebd4602b0a401086
samy884 <ssgill884@gmail.com>
2022-06-25 11:15:51 +0530

    redirecting to quotes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import AllQuotes from "./components/pages/AllQuotes";
 import NewQuote from "./components/pages/NewQuote";
 import QuoteDetails from "./components/pages/QuoteDetails";
@@ -6,6 +6,9 @@ import QuoteDetails from "./components/pages/QuoteDetails";
 function App() {
   return (
     <div>
+      <Route path="/">
+        <Redirect to="/quotes" />
+      </Route>
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
