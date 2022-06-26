d804d64f8cf37cdd30265acb2b7b6f8afe8a0b33
samy884 <ssgill884@gmail.com>
2022-06-26 13:51:47 +0530

    useHistory

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,8 +1,12 @@
 import QuoteForm from "../quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const onAddQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={onAddQuoteHandler} />;
 };
