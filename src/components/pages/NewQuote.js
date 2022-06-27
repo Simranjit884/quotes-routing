0f0fa5515732ec07d4d2f04e7c6c56b7ed9fe751
samy884 <ssgill884@gmail.com>
2022-06-27 15:17:53 +0530

    firebase post request

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,14 +1,29 @@
+import { useEffect } from "react";
 import QuoteForm from "../quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../../hooks/use-http";
+import { addQuote } from "../../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const onAddQuoteHandler = (quoteData) => {
-    console.log(quoteData);
-    history.push("/quotes");
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={onAddQuoteHandler} />;
+
+  return (
+    <QuoteForm
+      isLoading={status === "pending"}
+      onAddQuote={onAddQuoteHandler}
+    />
+  );
 };
 
 export default NewQuote;
