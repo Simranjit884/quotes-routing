d319e55c56f5c267e80c9b9e9375be899edf68d3
samy884 <ssgill884@gmail.com>
2022-06-27 13:06:24 +0530

    static button routing

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,18 +1,25 @@
-import { Fragment } from 'react';
-
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  console.log("location is--", location);
+  const queryParams = new URLSearchParams(location.search);
+  const isAscending = queryParams.get("sort") === "asc";
   const changeSortingHandler = () => {
-    
+    history.push("quotes/sort=asc");
   };
 
   return (
     <Fragment>
-    <div className={classes.sorting}>
-    <button onClick={changeSortingHandler}>Sort Ascending</button>
-    </div>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
