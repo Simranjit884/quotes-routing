59a3cbfdc0877e59f232dc43aace39d062e93155
samy884 <ssgill884@gmail.com>
2022-06-28 11:38:39 +0530

    sending request

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,30 +1,41 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
   const params = useParams();
-  const {quoteId} = params;
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedQuote } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
 
-  const addedCommnetHandler = () => {
-    
-  }
-  
+  const addedCommnetHandler = () => {};
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommnetHandler} />}
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommnetHandler}
+        />
+      )}
       <p>Comments...</p>
     </section>
   );
